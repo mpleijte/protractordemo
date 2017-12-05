8cf7cccff321c1f25646be1c9b04374544a550e2
mpleijte <pleijteit@gmail.com>
2017-12-05 20:29:07 +0000

    adding protractor conf and spec file

---
+++ conf.js
@@ -0,0 +1,7 @@
+// conf.js content e.g.
+exports.config = {
+  seleniumAddress:
+  'http://localhost:32772/wd/hub',
+  specs: ['spec.js']
+}
+
---
+++ spec.js
@@ -0,0 +1,8 @@
+// spec.js content e.g.
+describe('A feature spec', function () {
+  it('navigates to the site', function () {
+    browser.get('http://angularjs.org');
+    //browser.pause();
+  });
+});
+
